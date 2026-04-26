c39fb330323828070fd839b16eba44c6e62cc57a
varadnagapurkar01 <varadnagapurkar01@gmail.com>
2026-04-26 16:24:09 +0530

    image of rooms

--- public/app.js
+++ public/app.js
@@ -14,7 +14,7 @@ const galleryImages = {
     'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?w=800&auto=format&fit=crop',
     'https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?w=800&auto=format&fit=crop',
     'https://images.unsplash.com/photo-1566665797739-1674de7a421a?w=800&auto=format&fit=crop',
-    'https://images.unsplash.com/photo-1584622650111-993a426fbf0a?w=800&auto=format&fit=crop'
+    'https://images.unsplash.com/photo-1611892440504-42a792e24d32?w=800&auto=format&fit=crop'
   ],
   'Deluxe': [
     'https://images.unsplash.com/photo-1618773928121-c32242e63f39?w=800&auto=format&fit=crop',
@@ -26,7 +26,7 @@ const galleryImages = {
     'https://images.unsplash.com/photo-1590490360182-c33d57733427?w=800&auto=format&fit=crop',
     'https://images.unsplash.com/photo-1505693314120-0d443867891c?w=800&auto=format&fit=crop',
     'https://images.unsplash.com/photo-1566073771259-6a8506099945?w=800&auto=format&fit=crop',
-    'https://images.unsplash.com/photo-1583847268964-b28ce8fba08e?w=800&auto=format&fit=crop'
+    'https://images.unsplash.com/photo-1582719508461-905c673771fd?w=800&auto=format&fit=crop'
   ],
   'Presidential': [
     'https://images.unsplash.com/photo-1578683010236-d716f9a3f461?w=800&auto=format&fit=crop',
@@ -196,11 +196,27 @@ function openGallery(type) {
   document.getElementById('galleryContent').innerHTML = images.map(img => `
     <img src="${img}" class="gallery-img" alt="${type} room">
   `).join('');
+  document.getElementById('galleryBookBtn').onclick = () => bookFromGallery(type);
   document.getElementById('galleryModal').classList.remove('hidden');
 }
 
 function closeGallery() {
   document.getElementById('galleryModal').classList.add('hidden');
+}
+
+function bookFromGallery(type) {
+  closeGallery();
+  showPage('newbooking', document.getElementById('navNewBooking'));
+  
+  // Try to find and select a room of this type
+  const select = document.getElementById('roomSelect');
+  const options = Array.from(select.options);
+  const matchedOption = options.find(opt => opt.text.includes(type));
+  
+  if (matchedOption) {
+    select.value = matchedOption.value;
+    updateBillPreview();
+  }
 }
 
 // BILL PREVIEW
